b217d3f76ff96d6b9429e16d85898d7af57dc935
iBaq001 <backeedj@gmail.com>
2022-09-01 10:31:01 +0300

    личный кабинет история заказов

--- src/main/java/com/amr/project/service/abstracts/OrderService.java
+++ src/main/java/com/amr/project/service/abstracts/OrderService.java
@@ -1,6 +1,10 @@
 package com.amr.project.service.abstracts;
 
 import com.amr.project.model.entity.Order;
+import com.amr.project.model.entity.User;
+
+import java.util.List;
 
 public interface OrderService extends ReadWriteService<Order, Long>{
+
 }
